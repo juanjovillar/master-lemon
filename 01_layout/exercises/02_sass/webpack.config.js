dd82195eea8111b52e4162b5688b0fb4ed46c213
Juanjo Villar <juanjovillar@live.com>
2018-10-16 18:31:19 +0200

    Finished ex. 2

---
+++ 01_layout/exercises/02_sass/webpack.config.js
@@ -0,0 +1,89 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var webpack = require('webpack');
+
+var path = require('path');
+var basePath = __dirname;
+
+module.exports = {
+    context: path.join(basePath, 'src'),
+    entry: {
+        app: './scripts/index.js',
+        appStyles: [
+            './styles/styles.scss',
+        ],
+        vendor:[
+            '@babel/polyfill',
+            'jquery'
+        ],
+    },      
+    output: {
+        filename: '[name].[chunkhash].js',
+    },
+    optimization:{
+        splitChunks: {
+            cacheGroups:{
+                vendor:{
+                    chunks: 'initial',
+                    name: 'vendor',
+                    test: 'vendor',
+                    enforce: true, 
+                }
+            }
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',                
+            },
+            {
+                test:/\.scss$/,
+                use: [                   
+                    MiniCssExtractPlugin.loader, 
+                    "css-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [                   
+                    MiniCssExtractPlugin.loader, 
+                    "css-loader"
+                ],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
+            }
+        ],
+    },
+    devServer: {
+        port: 8080,
+    },
+    plugins: [
+        //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+        new HtmlWebpackPlugin({
+            filename: 'index.html', //Name of file in ./dist
+            template: 'index.html', //Name of the template in ./src
+            hash: true,
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: "[id].css"
+        }),
+    ]
+};
